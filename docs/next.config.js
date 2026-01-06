d662894c057d7cec2142b59702fb61a3e1f082f9
charlie <xyhp915@qq.com>
2026-01-06 10:06:59 +0800

    chore: update Next.js and TypeScript versions, adjust configuration settings

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-  reactStrictMode: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // Important: return the modified config
-    config.devServer = {
-      proxy: {
-        '/apis': {
-          target: 'http://127.0.0.1:8080',
-          pathRewrite: { '^/apis': '' }
-        }
-      }
-    }
-
-    return config
-  }
+  output: 'standalone',
+  distDir: 'dist', // Optional: changes the internal build directory name
 }
